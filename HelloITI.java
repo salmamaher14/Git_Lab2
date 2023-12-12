df32411237ec38f37d3c981f072583ccaa7d8a72
unknown <you@example.com>
2023-12-12 13:44:14 +0200

    Changing in HelloITI.java to print HelloGit

--- HelloITI.java
+++ HelloITI.java
@@ -12,7 +12,7 @@ package refrenceexamples;
 public class HelloITI {
    
         public static void main(String[] args) {  
-            System.out.println("Hello ITI");
+            System.out.println("HelloGit");
            
     }
     
